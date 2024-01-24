ed5653ede6abf681c6e4225910ef79df7fdea6aa
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2024-01-24 18:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const promise1 = new Promise((res,rej)=>{
+	setTimeout(()=>{res()},1000)
+})
+//your JS code here. If required.
+const promise2 = new Promise((res,rej)=>{
+	setTimeout(()=>{res()},2000)
+})
+//your JS code here. If required.
+const promise3 = new Promise((res,rej)=>{
+	setTimeout(()=>{res()},3000)
+})
+Promise.all([promise1,promise2,promise3]).then(data=>console.log(data))
+.catch(err=>console.log(err));
